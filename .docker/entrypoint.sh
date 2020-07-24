c1743b68faf3659f7b29db5ffe7cdbf3a63dd75d
Raphael Lorencini <raphaellorencini@gmail.com>
2020-07-24 09:15:56 -0300

    Correções no entrypoint do fullcycle_app

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 composer install
-docker -template /var/www/.docker/app/.env:/var/www/.env
-chmod -R 777 .env
-php artisan key:generate
+dockerize -template /var/www/.docker/app/.env:/var/www/.env
+chmod -R 777 /var/www/.env
 php artisan config:clear
+php artisan optimize:clear
+php artisan key:generate
 php artisan config:cache
 php artisan migrate
 php-fpm
